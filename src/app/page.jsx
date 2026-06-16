3b42306b8bad88f59518056238e599bae7c759c3
VanXodus305 <soumyadeep.30505@gmail.com>
2026-06-16 14:41:44 +0530

    improved folder structure

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,10 @@
 "use client";
-
-import LandingPage from "./pages/LandingPage";
+import Navigationbar from "./landing/components/Navigationbar";
+import HeroSection from "./landing/components/HeroSection";
+import FeaturesCarousel from "./landing/components/Features";
+import DashboardPreview from "./landing/components/Dashboard";
+import Footer from "./landing/components/Footer";
+import ScrollReveal from "./components/common/ScrollReveal";
 import { useEffect } from "react";
 
 export default function Home() {
@@ -8,5 +12,40 @@ export default function Home() {
     // Ensure page stays at top on load
     window.scrollTo(0, 0);
   }, []);
-  return <LandingPage />;
+  return (
+    <main className="overflow-hidden">
+      <section id="home">
+        <Navigationbar />
+        <HeroSection />
+      </section>
+
+      <section
+        id="features"
+        className="
+          relative
+          z-10
+          -mt-20
+          "
+      >
+        <ScrollReveal>
+          <FeaturesCarousel />
+        </ScrollReveal>
+      </section>
+
+      <section
+        id="dashboard"
+        className="
+          relative
+          z-20
+          -mt-20
+          "
+      >
+        <ScrollReveal>
+          <DashboardPreview />
+        </ScrollReveal>
+      </section>
+
+      <Footer />
+    </main>
+  );
 }
